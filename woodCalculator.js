ab41b680b4bfe36521e38915c211b2862d54005f
SShohag <shohag7m@gmail.com>
2020-07-17 15:48:00 +0600

    one file is remove

--- woodCalculator.js
+++
@@ -1,9 +0,0 @@
-name = ['mamun', 'rahim', 'karim', 'sorif', 'barkatullah', 'rony', 'pori', 'piu', 'sohan', 'sabina']
-var smallest = name[0];
-
-for (var i = 1; i < name.length; i++) {
-if (name[i] > smallest) {
-    smallest = name[i];
-  }
-}
-console.log(smallest);
