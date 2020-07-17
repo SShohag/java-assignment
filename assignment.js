e24de022b26ef0741b6fcc61a45e7aaa22b317ef
SShohag <shohag7m@gmail.com>
2020-07-17 18:27:17 +0600

    Three problems are solve

--- assignment.js
+++ assignment.js
@@ -1,4 +1,5 @@
 //Problem No.1 feetToMile
+
 function feetToMile(feet){
     var mile = feet/5280;
     return mile;
@@ -6,7 +7,9 @@ function feetToMile(feet){
 var result = feetToMile(5280);
 console.log("Your Answer is =", result, "mile",);
 
+
 // problem no.2 woodCalculator
+
 function woodCalculator(chair, table, bed){
     var needForChair = chair * 1;
     var needForTable = table * 3;
@@ -17,3 +20,18 @@ function woodCalculator(chair, table, bed){
   var woodResult = woodCalculator(1, 1, 1);
   console.log("Total wood needed = ", woodResult);
 
+
+//   Problem No 4 tinyFriend
+
+function tinyFriend(names){
+    var smallName = names[0];
+    for (var i = 0; i < names.length; i++){
+        var currentName = names[i];
+        if(currentName.length < smallName.length ){
+            smallName = currentName;
+        }
+    }
+    return smallName;
+}
+var result = tinyFriend(["saddam", "akmal","pio", "surovi", "jahangir", "kader"]);
+console.log("The smallest Name is : ", result);
